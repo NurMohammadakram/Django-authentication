fe3cf5901820abf2c4dcc57e7504861a231e7744
NurMohammadakram <nurmohammad19k8@gmail.com>
2025-12-27 20:33:07 +0600

    error solved

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.user_register, name='user_register'),
     path('login/', views.user_login, name='user_login')
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
 def user_register(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         
-        User.objects.create_user(username, password)
-        return redirect('home')
+        User.objects.create_user(username=username, password=password)
+        return redirect('user_login')
+    
     return render(request, 'auth/register.html')
 
 def user_login(request):
@@ -21,7 +24,7 @@ def user_login(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user:
             login(request, user)
             return HttpResponse('Succesfully loged in!')
         else:
